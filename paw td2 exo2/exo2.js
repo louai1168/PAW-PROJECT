6c761aa82f4e9c6b036b47d3f1940738120e8e19
louai1168 <louaibahidj7@gmail.com>
2025-11-22 15:54:41 +0100

    new version of td2 exo2

--- paw td2 exo2/exo2.js
+++ paw td2 exo2/exo2.js
@@ -38,3 +38,31 @@ document.getElementById("studentForm").addEventListener("submit", function(e) {
 
       
     });
+    function analyze() {
+  const rows = document.querySelectorAll("#attendance tbody tr");
+
+  rows.forEach(row => {
+    const inputs = row.querySelectorAll("input[type='checkbox']");
+    const sessions = Array.from(inputs).slice(0, 6);
+    //const participations = Array.from(inputs).slice(6, 12);
+
+    const absences = sessions.length - sessions.filter(c => c.checked).length;
+    //const parts = participations.filter(c => c.checked).length;
+
+    let message = "";
+    row.classList.remove("green", "yellow", "red");
+
+    if (absences < 3) {
+      row.classList.add("green");
+      message = "Good attendance – Excellent participation";
+    } else if (absences >= 3 && absences <= 4) {
+      row.classList.add("yellow");
+      message = "Warning – attendance low – You need to participate more";
+    } else {
+      row.classList.add("red");
+      message = "Excluded – too many absences – You need to participate more";
+    }
+
+    row.lastElementChild.textContent = `${absences} Abs /  ${message}`;
+  });
+}
